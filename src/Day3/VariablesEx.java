0955c5811632e2e21fc886b734506ed41ceeb809
shari7877 <rayudu.srihari@gmail.com>
2023-05-06 20:09:26 +0530

    changes done

--- src/Day3/VariablesEx.java
+++ src/Day3/VariablesEx.java
@@ -4,27 +4,27 @@ public class VariablesEx {
 	
 	public int i;// instance varibles
 	public static int j=1000;//static variable
+//sdfsdf
+//sdfsdf
+//sfsdfsdf
 
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+// TODO Auto-generated method stub
+// TODO Auto-generated method stub
+// TODO Auto-generated method stub
+// TODO Auto-generated method stub
      System.out.println(VariablesEx.j);
+     System.out.println(VariablesEx.j);
+System.out.println(VariablesEx.j);
      VariablesEx c1 =new VariablesEx();
-      System.out.println(VariablesEx.j);
-		
-		
-		 System.out.println(VariablesEx.j);
-		 System.out.println(VariablesEx.j);
-		 System.out.println(VariablesEx.j);
-		 System.out.println(VariablesEx.j);
+     
      System.out.println(c1.j);
-		
-	 System.out.println(VariablesEx.j);
-		 System.out.println(VariablesEx.j);
-		 System.out.println(VariablesEx.j);
-		
-		
+//// TODO Auto-generated method stub
+// TODO Auto-generated method stub
+// TODO Auto-generated method stub
 	}
 
 }
